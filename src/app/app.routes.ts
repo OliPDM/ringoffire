5dfd1b9cb37f776e1992bd35b57d73e6d57c5372
Oliver Geschine <oli.geschine@web.de>
2026-01-03 15:15:59 +0100

    add start-screen.component + game.component & first style on start-screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { StartScreenComponent } from './../app/start-screen/start-screen.component';
+import { GameComponent } from './../app/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StartScreenComponent },
+    { path: 'game', component: GameComponent }
+];
